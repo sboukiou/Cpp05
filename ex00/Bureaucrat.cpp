9286808379d2d8b8fc04262d00a8a2b22b52a69b
sboukiou <sboukiou@student.1337.ma>
2026-01-03 07:05:40 +0100

    Changed the bureaucrat unsinged to size_t + Fixed the critical issue of passing an int to a setGRade which expects a size_t and overflow it

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@ Bureaucrat::Bureaucrat(void): name("Un-Named"), grade(150) {
 	std::cout << "[INFO]: (Default Constructor){Bureaucrat}" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const std::string &param, const unsigned int &num): name(param), grade(num) {
+Bureaucrat::Bureaucrat(const std::string &param, const size_t &num): name(param), grade(num) {
 	if (num < 0)
 		throw GradeTooHighException();
 	if (num > 150)
@@ -24,7 +24,6 @@ Bureaucrat::~Bureaucrat(void) {
 void	Bureaucrat::operator=(const Bureaucrat &other) {
 	if (this == &other)
 		return ;
-	name = other.getName();
 	grade = other.getGrade();
 	std::cout << "[INFO]: (Assignment operator overload){Bureaucrat}" << std::endl;
 }
@@ -32,14 +31,11 @@ void	Bureaucrat::operator=(const Bureaucrat &other) {
 const std::string &Bureaucrat::getName(void) const {
 	return (name);
 }
-void	Bureaucrat::setName(const std::string &param) {
-	name = param;
-}
-const unsigned int &Bureaucrat::getGrade(void) const {
+const size_t &Bureaucrat::getGrade(void) const {
 	return (grade);
 }
-void	Bureaucrat::setGrade(const unsigned int &param) {
-	if (param < 0)
+void	Bureaucrat::setGrade(const int &param) {
+	if (param < 1)
 		throw GradeTooHighException();
 	if (param > 150)
 		throw GradeTooLowException();
@@ -48,23 +44,23 @@ void	Bureaucrat::setGrade(const unsigned int &param) {
 
 
 const char *Bureaucrat::GradeTooHighException::what(void) const throw() {
-	return ("Bureaucrat Exception: GradeTooHigh");
+	return ("{Bureaucrat Exception}: GradeTooHigh");
 }
 const char *Bureaucrat::GradeTooLowException::what(void) const throw() {
-	return ("Bureaucrat Exception: GradeTooLow");
+	return ("{Bureaucrat Exception}: GradeTooLow");
 }
 
 void	Bureaucrat::incrementGrade(void) {
 	if (grade > 0)
 		grade -= 1;
 	else
-		throw GradeTooLowException();
+		throw GradeTooHighException();
 }
 void	Bureaucrat::decrementGrade(void) {
 	if (grade < 150)
 		grade += 1;
 	else
-		throw GradeTooHighException();
+		throw GradeTooLowException();
 }
 
 
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -4,26 +4,25 @@
 #include <string>
 class Bureaucrat {
 	private:
-		std::string name;
-		unsigned int grade;
+		const std::string name;
+		size_t	grade;
 	public:
 		Bureaucrat();
-		Bureaucrat(const std::string &param, const unsigned int &num);
+		Bureaucrat(const std::string &param, const size_t &num);
 		Bureaucrat(const Bureaucrat &other);
 		~Bureaucrat();
 		void	operator=(const Bureaucrat &other);
 		const std::string &getName(void) const;
-		void	setName(const std::string &param);
-		const unsigned int &getGrade(void) const;
-		void	setGrade(const unsigned int &param);
+		const size_t &getGrade(void) const;
+		void	setGrade(const int &param);
 		void	incrementGrade(void);
 		void	decrementGrade(void);
 		// Nested Classes for the Exceptions
 		class GradeTooHighException: public std::exception {
 			public:
-				virtual const char *what() const throw();
+				virtual const char *what() const throw(); // Throw or noexcept means th function
+														  // will not throw any objects
 		};
-
 
 		class GradeTooLowException: public std::exception {
 			public:
