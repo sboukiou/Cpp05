ebf3053cbeab0bd37a389a9f076d56c70eb3e8f5
sboukiou <sboukiou@student.1337.ma>
2026-01-04 09:31:13 +0100

    Implemented the hardworking intern

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -1,15 +1,41 @@
 #include "./Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 #include <iostream>
+#include <cstdlib>
+
+AForm	*makePresident(const std::string &target) {
+	return new PresidentialPardonForm(target);
+}
+
+AForm	*makeShrubbery(const std::string &target) {
+	return new ShrubberyCreationForm(target);
+}
+
+AForm	*makeRobotomy(const std::string &target) {
+	return new RobotomyRequestForm(target);
+}
 
 Intern::Intern(void): name("Dummy-Intern") {
+
+	genericForm[0] = &makePresident;
+	genericForm[1] = &makeShrubbery;
+	genericForm[2] = &makeRobotomy;
 	std::cout << "[INFO]: (Default construction){Intern}\n";
 }
 
 Intern::Intern(const std::string &param): name(param) {
+	genericForm[0] = &makePresident;
+	genericForm[1] = &makeShrubbery;
+	genericForm[2] = &makeRobotomy;
 	std::cout << "[INFO]: (Parameterized construction){Intern}\n";
 }
 
 Intern::Intern(const Intern &other): name(other.name) {
+	genericForm[0] = &makePresident;
+	genericForm[1] = &makeShrubbery;
+	genericForm[2] = &makeRobotomy;
 	std::cout << "[INFO]: (Copy construction){Intern}\n";
 }
 
@@ -24,7 +50,27 @@ void	Intern::operator=(const Intern &other) {
 	std::cout << "[INFO]: (Assignment operator overload){Intern}\n";
 }
 
-AForm*	makeForm(const std::string &formName, const std::string &targetName) {
+const char *Intern::FormNotFoundException::what(void) const throw() {
+	return ("(InternException)[Form not Found !\n]");
+}
 
+AForm*	Intern::makeForm(const std::string &formName, const std::string &targetName) {
+	AForm *form = NULL;
+	const std::string FormsLabels[3] = {"PresidentialPardonForm"
+		, "ShrubberyCreationForm", "RobotomyRequestForm"};
+	for (int i = 0; i < 3; i += 1) {
+		if (targetName == FormsLabels[i]) {
+			std::cout << "Found it \n";
+			form = genericForm[i](targetName);
+			if (form == NULL) {
+				std::cerr << "Failed to make the form " << formName << std::endl;
+				exit(0);
+			}
+			return (form);
+		}
+	}
+	if (form == NULL)
+		throw Intern::FormNotFoundException();
+	return (NULL);
 }
 
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -7,6 +7,7 @@
 class Intern {
 	private:
 		std::string name;
+		AForm*	(*genericForm[3])(const std::string &target);
 	public:
 		Intern(void);
 		Intern(const std::string &name);
@@ -14,6 +15,11 @@ class Intern {
 		~Intern(void);
 		void	operator=(const Intern &other);
 		AForm*	makeForm(const std::string &formName, const std::string &targetName);
+		// Classes for Exceptions
+		class FormNotFoundException: public std::exception {
+			public:
+				const char*	what() const throw();
+		};
 
 };
 
